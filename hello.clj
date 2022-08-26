6e07991109bb29b8556ef71c241b747ff8b9a307
Benj <Benjamin.Schwerdtner@gmail.com>
2022-08-26 17:32:19 +0200

    Bigger example

--- hello.clj
+++ hello.clj
@@ -4,7 +4,14 @@
 
 (defn main []
   (when-not (fs/exists? "hurr") (fs/copy "hello.clj" "hurr"))
-  (println (->> (range) (take 10)))
-  (println (json/decode "{\"foo\": 10}")))
+  (println
+   (->> (range) (take 8) (map inc) (filter even?) (interleave (repeat 0))))
+  (println (json/decode "{\"foo\": 10}"))
+  (println
+   (->
+    (curl/get "https://postman-echo.com/get" {:query-params {"q" "clojure"}})
+    :body
+    (json/parse-string true)
+    :args)))
 
 (main)
